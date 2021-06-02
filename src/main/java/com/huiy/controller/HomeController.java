1c1256243b69ac885f79f0261d65281460920e4a
klloo <asdf016182@naver.com>
2021-06-02 23:38:36 +0900

    프로젝트생성_초기설정

--- src/main/java/com/huiy/controller/HomeController.java
+++ src/main/java/com/huiy/controller/HomeController.java
@@ -1,24 +1,34 @@
 package com.huiy.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.Data;
+import lombok.Setter;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	//위에서 DataSource를 빈으로 등록했기 때문에 의존성 주입 가능
+	@Setter(onMethod_ = {@Autowired})
+	private DataSource dataSource;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -32,6 +42,11 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		try(Connection con = dataSource.getConnection()){
+			System.out.println("성공");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
 		return "home";
 	}
---
+++ src/main/java/com/huiy/mapper/BoardMapper.java
@@ -0,0 +1,5 @@
+package com.huiy.mapper;
+
+public interface BoardMapper {
+
+}
